8fdb94e41d0ecf5b53ba095e966378911a881b5e
Ethan Yadan <152818660+ethan-yadan@users.noreply.github.com>
2024-12-14 20:39:56 +0200

    Update create_vpc.sh

--- create_vpc.sh
+++ create_vpc.sh
@@ -1,33 +1,16 @@
 #!/bin/bash 
 
-# Set variables 
-VPC_CIDR="10.0.0.0/16" # This VPC will have IP's ranging from 10.0.0.0 to 10.0.255.255
-REGION="us-east-1" # Sets the AWS desired region, where VPC and related resources wil be created  
-
-TAG_KEY="Name"
-TAG_VALUE="MyprojectVPC"
-# Tags for categorizing and managing resources 
-
 # Create a VPC
 function create_vpc(){
     echo "Creating VPC..."
-    VPC_ID=$(aws ec2 create-vpc --cidr-block $VPC_CIDR --region $REGION --query 'Vpc.VpcId' --output text 2>/dev/null)
-
-    if [ $? -ne 0 ]; then
-        echo "Failed to tag VPC"
-        return 1 
-    fi 
+    VPC_ID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --region us-east-1 --query 'Vpc.VpcId' --output text)
 
     echo "VPC created with ID: $VPC_ID"
 
     # Add a name tag to the VPC
     echo "Tagging VPC..."
-    aws ec2 create-tags --resources $VPC_ID --tags Key=$TAG_KEY,Value=$TAG_VALUE --region $REGION
-    if [ $? -ne 0 ]; then   
-        echo "Failed to tag VPC" 
-        return 1
-    fi 
-
+    aws ec2 create-tags --resources $VPC_ID --tags Key=name,Value=myProjectVPC --region us-east-1
+    
     echo "VPC tagged with $TAG_KEY=$TAG_VALUE"
 }
 
